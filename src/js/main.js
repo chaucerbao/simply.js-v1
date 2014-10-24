c8656f81f2c69e6ad7477a1f3c46a01ecb624121
Alvin Chan <aschan@gmail.com>
2014-10-23 17:36:22 -0700

    Reveal 'select' and 'ajax' libraries to public

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ var Simply = (function() {
   'use strict';
 
   return {
+    /* Libs */
+    select: require('./lib/selector.js'),
+    ajax: require('./lib/ajax.js'),
+
+    /* Modules */
     modal: require('./modules/modal.js'),
     tooltip: require('./modules/tooltip.js')
   };
